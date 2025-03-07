594e577c087a696781db7b61b5560be1beb8ef41
rodriquelali <rodriquelali@gmail.com>
2025-03-07 11:41:21 -0400

    Add empoints.

--- MyApplication3/app/src/main/java/com/example/primeropasoskotlin/models/services/ApiService.kt
+++ MyApplication3/app/src/main/java/com/example/primeropasoskotlin/models/services/ApiService.kt
@@ -1,14 +1,15 @@
 package com.example.primeropasoskotlin.models.services
 
 import com.example.primeropasoskotlin.models.Posts
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("/posts")
-    fun getPosts():Response<MutableList<Posts>>
-
+    @GET("posts")
+    fun getPosts():Call<List<Posts>>
     //mas empoints
-
+    @GET("/buscuedaFecha")
+    fun getFecha():Call<List<String>>
 
 }
