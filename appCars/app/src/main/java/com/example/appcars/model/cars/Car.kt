ee2fb69ed267677be3fbb4243c630e8ab1ef5b64
rodriquelali <rodriquelali@gmail.com>
2025-03-17 13:07:05 -0400

    Add proyect Cars.

---
+++ appCars/app/src/main/java/com/example/appcars/model/cars/Car.kt
@@ -0,0 +1,9 @@
+package com.example.appcars.model.cars
+
+data class Car (
+    val make: String,
+    val year: Int,
+    val model: Int,
+    val speed: Int,
+    val fuel: String
+    )
---
+++ appCars/app/src/main/java/com/example/appcars/model/users/User.kt
@@ -0,0 +1,8 @@
+package com.example.appcars.model.users
+
+data class User (
+    val first_name: String,
+    val last_name: String,
+    val email: String,
+    val password: String,
+)
