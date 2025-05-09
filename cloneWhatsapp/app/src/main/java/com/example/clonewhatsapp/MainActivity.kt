b169de2cb8bc83ae0bd19229fe9b030fb95371b3
rodriquelali <rodriquelali@gmail.com>
2025-05-09 10:52:30 -0400

    Add funcion optenerNumero.

--- cloneWhatsapp/app/src/main/java/com/example/clonewhatsapp/MainActivity.kt
+++ cloneWhatsapp/app/src/main/java/com/example/clonewhatsapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -58,13 +59,17 @@ class MainActivity : AppCompatActivity() {
             if (messageText.isNotEmpty()) {
                 val message = Message(
                     id = UUID.randomUUID().toString(),
-                    sendId = "user123",  // Aquí podrías usar el ID real del usuario logueado
+                    sendId = "admi123",  // Aquí podrías usar el ID real del usuario logueado
                     message = messageText
                 )
                 viewModel.sendMessage(message)
                 txtMessage.text.clear()
             }
         }
+    }
+
+    fun optenerNumer(){
+        Toast.makeText(this, "Optener numero....", Toast.LENGTH_SHORT).show()
     }
 }
 
